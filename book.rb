3995b4a7e01a88295592926eefd1e5a91709e77c
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 12:26:41 +0530

    add available? method in Book class

--- book.rb
+++ book.rb
@@ -4,5 +4,18 @@ class Book
     @author = author
     @available = true
   end
+  
+  def available?
+    if @available
+      puts "Book Available"
+    else
+      puts "Book not Available"
+    end
+  end
+
+  
+
 end
 
+# book1 = Book.new("Ruby basic", "David")
+# book1.available?
