644b5f083b50843b2f2271580dcacb11c4bbd1a5
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 15:39:59 +0530

    add test_display_book_when_exist

--- tests/test_library.rb
+++ tests/test_library.rb
@@ -4,7 +4,7 @@ require_relative "../library"
 class TestLibrary < Minitest::Test
   def setup
     @library = Library.new("City Library")
-    @book1 = Book.new("Ruby Basics", "David")
+    @book1 = Book.new("Ruby Basic", "David")
   end
 
   def test_library_name
@@ -15,7 +15,7 @@ class TestLibrary < Minitest::Test
     output = capture_io do
       @library.add_book(@book1)
     end
-    assert_equal "Ruby Basics Added successfully\n" , output[0]
+    assert_equal "Ruby Basic Added successfully\n" , output[0]
   end
 
   def test_dispaly_books_when_no_exist
@@ -26,6 +26,25 @@ class TestLibrary < Minitest::Test
     expected_output = <<~TEXT
       Library Name: City Library
       No Books Available
+    TEXT
+
+    assert_equal expected_output, output[0]
+  end
+
+  def test_display_books_when_exist
+    @library.add_book(@book1)
+
+    output = capture_io do
+      @library.display_books
+    end
+
+    expected_output = <<~TEXT
+      Library Name: City Library
+      Book Available
+      Title : Ruby Basic
+      Author : David
+      Status : Borrowed
+      ----------------------
     TEXT
 
     assert_equal expected_output, output[0]
