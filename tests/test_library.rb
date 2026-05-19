3c4fd1d9dfd71dd7f82614ab394bf6f3eca07ccc
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 15:14:26 +0530

    add test_dispaly_books_when_no_exist

--- tests/test_library.rb
+++ tests/test_library.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require_relative "../library"
-require_relative "../book"
 
 class TestLibrary < Minitest::Test
   def setup
@@ -17,5 +16,18 @@ class TestLibrary < Minitest::Test
       @library.add_book(@book1)
     end
     assert_equal "Ruby Basics Added successfully\n" , output[0]
+  end
+
+  def test_dispaly_books_when_no_exist
+    output = capture_io do 
+      @library.display_books
+    end
+
+    expected_output = <<~TEXT
+      Library Name: City Library
+      No Books Available
+    TEXT
+
+    assert_equal expected_output, output[0]
   end
 end
