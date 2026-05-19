c1dead0023dbdf5cba568eaacaba9b8bea8ddf53
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 15:02:27 +0530

    add test_add_book file for add_book method in library

--- tests/test_library.rb
+++ tests/test_library.rb
@@ -1,12 +1,21 @@
 require 'minitest/autorun'
 require_relative "../library"
+require_relative "../book"
 
 class TestLibrary < Minitest::Test
   def setup
     @library = Library.new("City Library")
+    @book1 = Book.new("Ruby Basics", "David")
   end
 
   def test_library_name
     assert_equal "City Library", @library.library_name
+  end
+
+  def test_add_book
+    output = capture_io do
+      @library.add_book(@book1)
+    end
+    assert_equal "Ruby Basics Added successfully\n" , output[0]
   end
 end
