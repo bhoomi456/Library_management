c8d13a3afa0faef10f4e1cc563a27fc34c264fa5
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 13:02:00 +0530

    make test_book_title test for book title

--- tests/test_book.rb
+++ tests/test_book.rb
@@ -3,6 +3,10 @@ require_relative "../book"
 
 class TestBook < Minitest::Test 
   def setup
-    book1 = Book.new("Ruby Basic", "David")
+    @book1 = Book.new("Ruby Basic", "David")
+  end
+  
+  def test_book_title
+    assert_equal "Ruby Basic", @book1.title
   end
 end
