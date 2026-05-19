8ca59a4a225a04c958b239e22a040b69dd9035f7
bhoomi456 <bhoomij0304@gmail.com>
2026-05-19 14:13:41 +0530

    add test_library_name for library name

--- tests/test_library.rb
+++ tests/test_library.rb
@@ -3,6 +3,10 @@ require_relative "../library"
 
 class TestLibrary < Minitest::Test
   def setup
-    library = Library.new("City Library")
+    @library = Library.new("City Library")
+  end
+
+  def test_library_name
+    assert_equal "City Library", @library.library_name
   end
 end
