6e03a097a0a7ca11f562de28011c72316c9e119f
bhoomi456 <bhoomij0304@gmail.com>
2026-05-20 16:52:37 +0530

    add borrowed books count in library

--- main.rb
+++ main.rb
@@ -5,14 +5,19 @@ require_relative "member"
 book1 = Book.new("Ruby Basic", "David")
 book2 = Book.new("Rails Guide", "Roy")
 book3 = Book.new("Python", "Roben")
+book4 = Book.new("Java", "David")
+
 
 library = Library.new("City Library")
+member = Member.new("Bhoomi")
 library.display_books
 puts "----------------------"
 
 library.add_book(book1)
 library.add_book(book2) #book already exist, we don't add same book again
 library.add_book(book3)
+library.add_book(book4)
+
 
 # library.add_book(book2)
 
@@ -29,15 +34,23 @@ library.return_book("Ruby Basic")
 puts "----------------------"
 library.display_books
 
-member = Member.new("Bhoomi")
-member.borrowed_book(book2)
-member.borrowed_book(book1)
-member.borrowed_book(book3)
-library.available_books_count
 
+member.borrowed_book(book2)
+puts "----------------------"
+member.borrowed_book(book1)
+puts "----------------------"
+member.borrowed_book(book3)
+puts "----------------------"
+library.available_books_count
+puts "----------------------"
+library.borrowed_books_count
+puts "----------------------"
 library.display_books
 
 member.returend_book(book2)
+puts "----------------------"
+
 library.display_books
+puts "----------------------"
 library.search_by_author("David")
 
