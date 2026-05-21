136ec1f629cedcc661a26e8a654f344c5fb19c68
bhoomi456 <bhoomij0304@gmail.com>
2026-05-21 17:49:16 +0530

    add changes in find book and update amin.rb

--- library.rb
+++ library.rb
@@ -8,23 +8,23 @@ class Library
   end
 
   def add_member(member)
-    existing_member = find_member(member)
+    existing_member = find_member(member.name)
     if existing_member
       puts "Member Already Exist"
     else
       @members << member
-      puts "#{member} Added Successfully "
+      puts "#{member.name} Added Successfully "
     end
   end
 
   def find_member(name)
-    @members.find { |member| member.downcase == name.downcase }
+    @members.find { |member| member.name.strip.downcase == name.strip.downcase }
   end
 
   def add_book(book)
     existing_book = @books.find do |b|  #Prevent duplicate books with the same title.
 
-      b.title.downcase == book.title.downcase
+      b.title.strip.downcase == book.title.strip.downcase
     end
     if existing_book
       puts "Book Already Exist"
@@ -52,16 +52,14 @@ class Library
   end
 
   def find_book(title)
-    found = @books.find {|book| book.title.downcase == title.downcase}
+    found = @books.find {|book| book.title.strip.downcase == title.strip.downcase}
     if found
       found
-    else
-      puts "Not found"
     end
   end
 
   def search_by_author(author)
-    books = @books.select { |book| book.author.downcase == author.downcase }
+    books = @books.select { |book| book.author.strip.downcase == author.strip.downcase }
     if books.empty?
       puts "No Books found"
     else
@@ -101,6 +99,7 @@ class Library
       puts "#{book.title} borrowed by #{member.name}"
     end
   end
+
   def return_book(title, member)
     book = find_book(title)
 
